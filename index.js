01cfd3d484ce1f9ec4892590408c01100779e14b
jsm4r1 <mariam.ozuna26@compscihigh.org>
2025-09-09 22:46:31 +0000

    Answered question 1: what is version control

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Version Control Assessment
 // 1. Explain what version control is.
 // Write your answer as a string and store it in the variable below:
-let versionControlExplanation = "";
+let versionControlExplanation = "Version control helps keep track of modifications and allows you to revert to old versions of your code";
 
 console.log("What is version control?");
 console.log(versionControlExplanation); 
@@ -9,20 +9,26 @@ console.log(versionControlExplanation);
 // In the terminal, run "node index.js" to run and test your code.
 
 /* 
-   At this point, commit your work with an appropriate commit message.
+   At this point, commit your work with an appropriate commit message. 
    After committing, proceed to the next questions.
 */
 
 // 2. Explain what a Git repository is.
 // Write your answer as a string and store it in the variable below:
-let gitRepoExplanation = "";
+let gitRepoExplanation = "a git repo is a storage space that contains all the files of a project along with their complete version history";
 
 console.log("What is a Git repository?");
 console.log(gitRepoExplanation); 
 
 // 3. Name the benefits of using version control.
 // Write your answer as an array of strings below, with each string being a benefit.
-let versionControlBenefits = [];
+let versionControlBenefits = [
+   "keeps a history of changes",
+   "allows collaboration among multiple contributors",
+   "able to revert to olf versions of code",
+   "reduces risk of losing work"
+];
+
 
 console.log("What are the benefits of using version control?");
 console.log(versionControlBenefits); 
@@ -34,7 +40,11 @@ console.log(versionControlBenefits);
 
 // 4. Explain the steps to pushing changes in Git.
 // Write your answer as an array of steps below, with each step being a string.
-let stepsToPushChanges = [];
+let stepsToPushChanges = [
+   "git add . (Stage changes)",
+   "git commit -m 'your commit message' (commit the staged changes)",
+   "git push origin main (push your commits to the remote repository)",
+];
 
 console.log("What are the steps to pushing changes in Git?");
 console.log(stepsToPushChanges); 
